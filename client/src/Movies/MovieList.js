223dbf63a67a166094d2d1bcd0109ac6c417be92
tatiana faramarzi <tfaramarzi@gmail.com>
2019-07-22 13:47:51 -0700

    Completed project setup and added id parameter to movies route

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,7 +31,8 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to="/movies/:id">
+      <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,7 +47,8 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+      </div>
+    </Link>
   );
 }
 
